dfb04939454889df471686e9fda641a966b19e3f
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-03-24 23:45:04 +0330

    main.py

--- src/main.py
+++ src/main.py
@@ -1,40 +1,41 @@
 import json
 import random
+import requests
+import os
+from datetime import datetime
 
-def load_ips():
+def fetch_ips():
     try:
-        with open("ips.json", "r") as f:
-            ips = json.load(f)
-            if not ips["ipv4"]:
-                ips["ipv4"] = ["162.159.192.1:2408", "162.159.193.1:500"]
-            if not ips["ipv6"]:
-                ips["ipv6"] = ["[2606:4700:d0::a29f:c001]:2408", "[2606:4700:d1::a29f:c001]:500"]
-            return ips
-    except:
+        response = requests.get('https://raw.githubusercontent.com/ircfspace/endpoint/refs/heads/main/ip.json')
+        data = response.json()
+        return data
+    except Exception as e:
         return {
-            "ipv4": ["162.159.192.1:2408", "162.159.193.1:500"],
-            "ipv6": ["[2606:4700:d0::a29f:c001]:2408", "[2606:4700:d1::a29f:c001]:500"]
+            "ipv4": ["162.159.192.23:859", "162.159.192.178:4500"],
+            "ipv6": ["[2606:4700:d1::9f62:b405:88e4:858e]:7152"]
         }
 
 def generate_config(ip_port, index, ip_version):
-    return f"warp://{ip_port}/?ifp=50-100&ifps=50-100&ifpd=3-6&ifpm=m4&&detour=warp://{ip_port}/?ifp=50-100&ifps=50-100&ifpd=3-6&ifpm=m4#Anon-WoW-{ip_version}🇩🇪-{index}"
+    base = f"warp://{ip_port}/?ifp=50-100&ifps=50-100&ifpd=3-6&ifpm=m4&&detour=warp://{ip_port}/?ifp=50-100&ifps=50-100&ifpd=3-6&ifpm=m4#Anon-WoW-{ip_version}🇩🇪-{index}"
+    return base
 
 def update_config_file():
-    with open("src/templates/config_template.txt", "r", encoding="utf-8") as f:
+    with open('src/templates/config_template.txt', 'r', encoding='utf-8') as f:
         template = f.read()
 
-    ips = load_ips()
-    ipv4 = random.choice(ips["ipv4"])
-    ipv6 = random.choice(ips["ipv6"])
+    ips = fetch_ips()
+    
+    ipv4_list = random.sample(ips['ipv4'], 2)
+    ipv6 = random.choice(ips['ipv6'])
     
     configs = []
-    configs.append("warp://auto")
-    configs.append(generate_config(ipv4, 1, "IPv4"))
-    configs.append(generate_config(ipv6, 2, "IPv6"))
+    configs.append(generate_config(ipv4_list[0], 1, "IPv4"))
+    configs.append(generate_config(ipv4_list[1], 2, "IPv4"))
+    configs.append(generate_config(ipv6, 3, "IPv6"))
     
     final_config = template + "\n" + "\n\n".join(configs)
     
-    with open("config.txt", "w", encoding="utf-8") as f:
+    with open('config.txt', 'w', encoding='utf-8') as f:
         f.write(final_config)
 
 if __name__ == "__main__":
